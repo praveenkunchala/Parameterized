1e4ab3f1cd98ccd3ca7e65a903080d63243b79d1
bhanugavinihub <bhanu.chandu@progressive-tech.co.uk>
2021-02-08 12:30:17 +0530

    last

--- src/main/java/com/utility/Baseclass.java
+++ src/main/java/com/utility/Baseclass.java
@@ -72,7 +72,7 @@ public class Baseclass {
 		switch (browserType) {
 		case "chrome":
 			 WebDriverManager.chromedriver().setup();
-			// WebDriverManager.chromedriver().driverVersion("72.0.3626.81").setup();
+			 WebDriverManager.chromedriver().driverVersion("72.0.3626.81").setup();
 			// System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 			/* ChromeOptions options = new ChromeOptions();
 			 options.addArguments("headless");
