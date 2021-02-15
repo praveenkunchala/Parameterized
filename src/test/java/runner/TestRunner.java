ee328728b75f166bda6cac0335935b6f5858ecbf
bhanugavinihub <bhanu.chandu@progressive-tech.co.uk>
2021-02-15 13:44:49 +0530

    last

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 				glue = { "stepDefination" }, 
 				monochrome = true, 
 				plugin = {"pretty","html:target/cucumber", "json:target/cucumber.json"},
-				tags = {"@PCC_All_Article_Page,@PCC_Verify_individual_Article_page"}
+				tags = {"@PCC_Verify_individual_Article_page"}
 )
 		public class TestRunner {
 			
