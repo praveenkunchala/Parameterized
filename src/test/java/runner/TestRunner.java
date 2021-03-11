14a5a37524cadc4e7576c4c22ec082dabd1cda96
vinodkumargogisetty007 <vinod.gogisetty@progressive-tech.co.uk>
2021-03-11 16:06:36 +0530

    All

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 				monochrome = true, 
 				plugin = {"pretty","html:target/cucumber", "json:target/cucumber.json"},
 
-				tags = {"@Preference_Categories"}
+				tags = {"@Account_Mobile_Preference_Link"}
 
 )
 		public class TestRunner {
